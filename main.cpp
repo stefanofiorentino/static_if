8edbe29dc4285a24ba2614e1280292cc340c4480
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-02-10 23:54:40 +0100

    Initial commit
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-#include <type_traits>
 
 // https://baptiste-wicht.com/posts/2015/07/simulate-static_if-with-c11c14.html
 // https://lists.boost.org/Archives/boost/2014/08/216607.php
 
-template <bool B, typename T = void>
-using enable_if_t = typename std::enable_if<B, T>::type;
-
-
-template<typename T, enable_if_t<!std::is_same<std::string, T>::value, int> = 42>
+template<typename T, std::enable_if_t<!std::is_same<std::string, T>::value, int> = 42>
 void decrement_kindof(T &value)
 {
     --value;
 }
 
-template<typename T, enable_if_t<std::is_same<std::string, T>::value, int> = 42>
+template<typename T, std::enable_if_t<std::is_same<std::string, T>::value, int> = 42>
 void decrement_kindof(T &value)
 {
     value.pop_back();
