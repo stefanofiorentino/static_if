c9743e166468099473a9ee7dccb74719831804ed
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-02-10 23:56:49 +0100

    solution proposed by Baptiste Witch in c++14
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,16 +3,53 @@
 // https://baptiste-wicht.com/posts/2015/07/simulate-static_if-with-c11c14.html
 // https://lists.boost.org/Archives/boost/2014/08/216607.php
 
-template<typename T, std::enable_if_t<!std::is_same<std::string, T>::value, int> = 42>
-void decrement_kindof(T &value)
-{
-    --value;
+namespace static_if_detail {
+
+    struct identity {
+        template<typename T>
+        T operator()(T&& x) const {
+            return std::forward<T>(x);
+        }
+    };
+
+    template<bool Cond>
+    struct statement {
+        template<typename F>
+        void then(const F& f){
+            f(identity());
+        }
+
+        template<typename F>
+        void else_(const F&){}
+    };
+
+    template<>
+    struct statement<false> {
+        template<typename F>
+        void then(const F&){}
+
+        template<typename F>
+        void else_(const F& f){
+            f(identity());
+        }
+    };
+
+} //end of namespace static_if_detail
+
+template<bool Cond, typename F>
+static_if_detail::statement<Cond> static_if(F const& f){
+    static_if_detail::statement<Cond> if_;
+    if_.then(f);
+    return if_;
 }
 
-template<typename T, std::enable_if_t<std::is_same<std::string, T>::value, int> = 42>
-void decrement_kindof(T &value)
-{
-    value.pop_back();
+template<typename T>
+void decrement_kindof(T& value){
+    static_if<std::is_same<std::string, T>::value>([&](auto f){
+        f(value).pop_back();
+    }).else_([&](auto f){
+        --f(value);
+    });
 }
 
 int main()
