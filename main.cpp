a8bfe907ab1678fe8a1a1bc35e8f09150bdb4b97
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-02-18 23:11:49 +0100

    solution proposed by Baptiste Witch in c++14
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,33 +3,42 @@
 // https://baptiste-wicht.com/posts/2015/07/simulate-static_if-with-c11c14.html
 // https://lists.boost.org/Archives/boost/2014/08/216607.php
 
-namespace static_if_detail {
+namespace static_if_detail
+{
 
-    struct identity {
+    struct identity
+    {
         template<typename T>
-        T operator()(T&& x) const {
+        T operator()(T &&x) const
+        {
             return std::forward<T>(x);
         }
     };
 
     template<bool Cond>
-    struct statement {
+    struct statement
+    {
         template<typename F>
-        void then(const F& f){
+        void then(const F &f)
+        {
             f(identity());
         }
 
         template<typename F>
-        void else_(const F&){}
+        void else_(const F &)
+        {}
     };
 
     template<>
-    struct statement<false> {
+    struct statement<false>
+    {
         template<typename F>
-        void then(const F&){}
+        void then(const F &)
+        {}
 
         template<typename F>
-        void else_(const F& f){
+        void else_(const F &f)
+        {
             f(identity());
         }
     };
@@ -37,19 +46,24 @@ namespace static_if_detail {
 } //end of namespace static_if_detail
 
 template<bool Cond, typename F>
-static_if_detail::statement<Cond> static_if(F const& f){
+static_if_detail::statement<Cond> static_if(F const &f)
+{
+    std::puts(__PRETTY_FUNCTION__);
     static_if_detail::statement<Cond> if_;
     if_.then(f);
     return if_;
 }
 
 template<typename T>
-void decrement_kindof(T& value){
-    static_if<std::is_same<std::string, T>::value>([&](auto f){
-        f(value).pop_back();
-    }).else_([&](auto f){
-        --f(value);
-    });
+void decrement_kindof(T &value)
+{
+    static_if<std::is_same<std::string, T>::value>([&](auto f)
+                                                   {
+                                                       f(value).pop_back();
+                                                   }).else_([&](auto f)
+                                                            {
+                                                                --f(value);
+                                                            });
 }
 
 int main()
